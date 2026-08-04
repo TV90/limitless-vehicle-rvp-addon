be85e86a78147bd3117a460c1e5b2902a8450c39
DTC10E <2361166274@QQ.COM>
2026-08-04 20:38:36 +0800

    GPS目标设定距离限制改回65535

--- src/main/java/org/ywzj/rvp/network/C2SSetGPSTarget.java
+++ src/main/java/org/ywzj/rvp/network/C2SSetGPSTarget.java
@@ -103,7 +103,7 @@ public class C2SSetGPSTarget {
                 RVP_Network.CHANNEL.sendTo(S2CGpsStateSync.of(snapshot), player.connection.connection, net.minecraftforge.network.NetworkDirection.PLAY_TO_CLIENT);
                 return;
             }
-            double maxDist = 4096.0;
+            double maxDist = 65535.0;
             Vec3 target = new Vec3(msg.x, msg.y, msg.z);
             if (player.position().distanceToSqr(target) > maxDist * maxDist) {
                 snapshot = GPSTargetManager.snapshot(player);
